62af1e117d09fb6311c1aa29449a87bf98b300a6
Nicolas Murphy <murphynick10@gmail.com>
2023-12-20 16:08:53 -0600

    delete works

--- api/delete.php
+++ api/delete.php
@@ -1,34 +1,53 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // Only for development!
+header('Access-Control-Allow-Methods: DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 require 'db_connect.php';
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Preflight request, just return 200 OK
+    http_response_code(200);
+    exit;
+}
 
-    // Prepare a delete statement
-    $sql = "DELETE FROM appointments WHERE id = :id";
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // Parse the input to get the appointment ID
+    parse_str(file_get_contents("php://input"), $input);
+    $id = $input['id'] ?? null;
 
-    if ($stmt = $conn->prepare($sql)) {
-        // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    if ($id) {
+        // Prepare a delete statement
+        $sql = "DELETE FROM appointments WHERE id = :id";
 
-        // Attempt to execute the prepared statement
-        if ($stmt->execute()) {
-            echo "Appointment deleted successfully.";
+        if ($stmt = $conn->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                echo json_encode(['message' => 'Appointment deleted successfully.']);
+            } else {
+                http_response_code(500);
+                echo json_encode(['error' => 'Error deleting appointment.']);
+            }
         } else {
-            echo "Error deleting appointment.";
+            http_response_code(500);
+            echo json_encode(['error' => 'Error preparing statement.']);
         }
-    }
 
-    // Close statement
-    unset($stmt);
+        // Close statement
+        unset($stmt);
+    } else {
+        http_response_code(400);
+        echo json_encode(['error' => 'No ID provided for deletion.']);
+    }
 } else {
-    echo "No ID provided for deletion.";
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed.']);
 }
 
 // Close connection
 unset($conn);
-
-// Redirect to index.php
-header('Location: index.php');
-exit;
 ?>
--- appointment-tracker-frontend/src/AppointmentList.tsx
+++ appointment-tracker-frontend/src/AppointmentList.tsx
@@ -11,23 +11,45 @@ const AppointmentList: React.FC = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchAppointments = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/api/index.php');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setAppointments(data);
-      } catch (error) {
-        console.error('Fetch error:', error);
+  const fetchAppointments = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('http://localhost:8000/api/index.php');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-      setIsLoading(false);
-    };
+      const data = await response.json();
+      setAppointments(data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+    setIsLoading(false);
+  };
 
+  useEffect(() => {
     fetchAppointments();
   }, []);
+
+  const deleteAppointment = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/delete.php`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `id=${id}`,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      await response.json();
+      await fetchAppointments(); // Refetch the appointments after deletion
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -42,6 +64,7 @@ const AppointmentList: React.FC = () => {
             <h2>{appointment.title}</h2>
             <p>{appointment.date}</p>
             {/* Add more details here */}
+            <button onClick={() => deleteAppointment(appointment.id)}>Delete</button>
           </div>
         ))
       ) : (
