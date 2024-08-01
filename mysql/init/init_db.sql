479a6e1009b48947c8aa65be9e32e3aa0752af6c
Nicolas Murphy <murphynick10@gmail.com>
2024-07-31 19:58:07 -0500

    sql initialization doesn't include seconds, formatted

--- mysql/init/init_db.sql
+++ mysql/init/init_db.sql
@@ -1,14 +1,17 @@
 CREATE DATABASE IF NOT EXISTS mydatabase;
+
 USE mydatabase;
 
-CREATE TABLE appointments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    address TEXT,
-    date DATE NOT NULL,
-    time TIME NOT NULL
-);
 
--- Insert initial data
-INSERT INTO appointments (title, description, address, date, time) VALUES ('Sample Appointment', 'This is a test appointment.', 'Sample Address', CURDATE(), CURTIME());
+CREATE TABLE appointments (id INT AUTO_INCREMENT PRIMARY KEY,
+                                                         title VARCHAR(255) NOT NULL,
+                                                                            description TEXT, address TEXT, date DATE NOT NULL,
+                                                                                                                      time TIME NOT NULL);
+
+
+INSERT INTO appointments (title, description, address, date, time)
+VALUES ('Sample Appointment',
+        'This is a test appointment.',
+        'Sample Address',
+        CURDATE(),
+        TIME_FORMAT(CURTIME(), '%H:%i'));
