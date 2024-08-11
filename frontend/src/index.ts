8b9701395d2bca312827db2757cf8b694796093e
Nicolas Murphy <murphynick10@gmail.com>
2024-08-11 15:16:34 -0500

    ready to start recreating

--- frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-const message: string = "Hello from TypeScript!!";
-console.log(message);
-
-const messageElement = document.getElementById("message");
-if (messageElement) {
-  messageElement.innerText = message;
-} else {
-  console.error('Element with id "message" not found');
-}
--- public/index.php
+++ public/index.php
@@ -4,15 +4,18 @@ echo "PHP is working!";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My App</title>
+    <title>Appointment Tracker PHP</title>
 </head>
+
 <body>
     <div id="app"></div>
     <div>hey</div>
 
     <script type="module" src="../js/index.js"></script>
 </body>
+
 </html>
