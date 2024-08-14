552bba5d963825cb5db7d13518ad7d0bd1e1e53f
Nicolas Murphy <murphynick10@gmail.com>
2024-08-13 20:19:29 -0500

    .env for mysql

--- php/list-appointments.php
+++ php/list-appointments.php
@@ -1,8 +1,13 @@
 <?php
-$host = 'mysql';
-$dbname = 'mydatabase';
-$user = 'user';
-$pass = 'password';
+require '/var/www/html/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html');
+$dotenv->load();
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
