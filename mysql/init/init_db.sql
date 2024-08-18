e38379ca88005c398ea5d0b13571fa53303e47cf
Nicolas Murphy <murphynick10@gmail.com>
2024-08-17 20:21:52 -0500

    updated database to use new values

--- mysql/init/init_db.sql
+++ mysql/init/init_db.sql
@@ -2,24 +2,39 @@ CREATE DATABASE IF NOT EXISTS mydatabase;
 
 USE mydatabase;
 
+CREATE TABLE IF NOT EXISTS appointments (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        client VARCHAR(255) NOT NULL,
+        caregiver VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        date DATE NOT NULL,
+        startTime TIME NOT NULL,
+        endTime TIME NOT NULL,
+        notes VARCHAR(1000)
+);
 
-CREATE TABLE IF NOT EXISTS appointments (id INT AUTO_INCREMENT PRIMARY KEY,
-                                                                       title VARCHAR(255) NOT NULL,
-                                                                                          description TEXT, address TEXT, date DATE NOT NULL,
-                                                                                                                                    time TIME NOT NULL);
+INSERT INTO
+        appointments (client, caregiver, address, date, startTime, endTime, notes)
+VALUES
+        (
+                'John Doe',
+                'Jane Smith',
+                '123 Address Lane',
+                CURDATE(),
+                TIME_FORMAT(CURTIME(), '%H:%i'),
+                TIME_FORMAT(CURTIME() + INTERVAL 1 HOUR, '%H:%i'),
+                'notes...'
+        );
 
-
-INSERT INTO appointments (title, description, address, date, time)
-VALUES ('B',
-        'Description 1',
-        '123',
-        CURDATE(),
-        TIME_FORMAT(CURTIME(), '%H:%i'));
-
-
-INSERT INTO appointments (title, description, address, date, time)
-VALUES ('A',
-        'Description 2',
-        '456',
-        CURDATE() + INTERVAL 1 DAY,
-                               TIME_FORMAT(CURTIME() + INTERVAL 1 HOUR, '%H:%i'));
+INSERT INTO
+        appointments (client, caregiver, address, date, startTime, endTime, notes)
+VALUES
+        (
+                'Alice Johnson',
+                'Bob Brown',
+                '456 Street Street',
+                CURDATE() + INTERVAL 1 DAY,
+                TIME_FORMAT(CURTIME(), '%H:%i'),
+                TIME_FORMAT(CURTIME() + INTERVAL 1 HOUR, '%H:%i'),
+                'notes...'
+        );
